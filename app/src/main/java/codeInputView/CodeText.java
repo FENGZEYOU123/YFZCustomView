6326c07027643c18d55786f057d8dbb25f310a82
FENGZEYOU123 <35758413+FENGZEYOU123@users.noreply.github.com>
2021-02-23 16:49:11 +0800

    闪烁光标定时显示-每次新下坐标显示

--- app/src/main/java/codeInputView/CodeText.java
+++ app/src/main/java/codeInputView/CodeText.java
@@ -25,7 +25,7 @@ import utils.YFZDisplayUtils;
 public class CodeText extends androidx.appcompat.widget.AppCompatEditText {
     private final String TAG= CodeText.class.getName();
     private final int PAINT_FILLED =100, PAINT_STROKE =101;
-    private final int CODE_TEXT_STYLE_NORMAL=200,CODE_TEXT_STYLE_HIGHLIGHT=201;
+//    private final int CODE_TEXT_STYLE_NORMAL=200,CODE_TEXT_STYLE_HIGHLIGHT=201;
     private final String DEFAULT_HIDE_CONTENT="*";
     private Context mContext;
     private int measureMode=0;
@@ -34,7 +34,7 @@ public class CodeText extends androidx.appcompat.widget.AppCompatEditText {
     //组件
     private int mViewBackgroundColor =Color.TRANSPARENT;//背景颜色
     private Drawable mViewBackgroundDrawable;//背景Drawable
-    private int mCodeStyle=CODE_TEXT_STYLE_NORMAL;//组件模式 （正常，高光）
+//    private int mCodeStyle=CODE_TEXT_STYLE_NORMAL;//组件模式 （正常，高光）
     private boolean mEnableHideCode =false;//是否隐藏输入code
     private String mHideCodeString;//隐藏输入code-显示的内容
     //盒子
@@ -55,6 +55,7 @@ public class CodeText extends androidx.appcompat.widget.AppCompatEditText {
     private String[] mCodeArray;//输入Code内容
     //高亮
     private Paint mHighLightPaint;//笔刷
+    private boolean mEnableHighLight=false;//是否开启高亮
     private int mHighLightIndex =0;//下坐标
     private int mHighLightBackgroundColor=Color.BLUE;//颜色
     private int mHighLightStrokeStyle = PAINT_STROKE;//高亮样式（空心，实心）
@@ -62,13 +63,17 @@ public class CodeText extends androidx.appcompat.widget.AppCompatEditText {
     private float mHighLightRadius=5f;//圆弧半径
     //光标-笔刷
     private Paint mCursorPaint;//笔刷
-    private int mCursorStrokeWidth=4;//宽度
+    private int mCursorStrokeWidth=1;//宽度
     private int mCursorColor=Color.BLACK;//颜色
-    private boolean mCursorEnable =true;//是否显示
-    private boolean mCursorDisplaying =false;//显示-定时器-闪烁效果
+    private boolean mCursorEnable =true;//是否开启光标
+    private boolean mCursorDisplayingByTimer =false;//显示光标-定时器-闪烁效果
+    private boolean mCursorDisplayingByIndex =false;//显示光标-第一次下坐标
     private Timer mCursorTimer;//定时器
     private TimerTask mCursorTimerTask;//定时器任务
-    
+    private int mCursorHeightPadding=1;//上下边距
+    private int mCursorFrequency=500;//闪烁频率
+
+
     public CodeText(@NonNull Context context) {
         super(context);
         initial(context);
@@ -78,7 +83,11 @@ public class CodeText extends androidx.appcompat.widget.AppCompatEditText {
 
         TypedArray typedArray=context.obtainStyledAttributes(attrs, R.styleable.CodeText);
         //view
-        mCodeStyle=typedArray.getInt(R.styleable.CodeText_codeText_Style,mCodeStyle);//View的样式
+//                 <attr name="codeText_Style" format="integer">
+//            <enum name="Normal" value="200" />
+//            <enum name="Highlight" value="201" />
+//        </attr>
+//        mCodeStyle=typedArray.getInt(R.styleable.CodeText_codeText_Style,mCodeStyle);//View的样式
         mViewBackgroundColor =typedArray.getInt(R.styleable.CodeText_codeText_backgroundColor, mViewBackgroundColor);//View背景颜色
         mViewBackgroundDrawable=typedArray.getDrawable(R.styleable.CodeText_codeText_backgroundDrawable);//View背景Drawable
         //盒子
@@ -95,6 +104,7 @@ public class CodeText extends androidx.appcompat.widget.AppCompatEditText {
         mHighLightStrokeStyle =typedArray.getInt(R.styleable.CodeText_codeText_highLightStrokeStyle, mBoxStrokeStyle);//笔刷样式-默认跟盒子一样
         mHighLightStrokeWidth=typedArray.getInt(R.styleable.CodeText_codeText_highLightStrokeWidth, mBoxStrokeWidth);//空心线粗细-默认跟盒子一样
         mHighLightRadius=typedArray.getFloat(R.styleable.CodeText_codeText_highLightRadius,mBoxRadius);//圆弧半径-默认跟盒子一样
+        mEnableHighLight=typedArray.getBoolean(R.styleable.CodeText_codeText_enableHighLight,mEnableHighLight);//开启关闭
         //控制
         mEnableHideCode =typedArray.getBoolean(R.styleable.CodeText_codeText_enableHideCode, mEnableHideCode);//是否隐藏输入内容
         mHideCodeString=typedArray.getString(R.styleable.CodeText_codeText_enableHideCode_displayContent);//隐藏内容时-显示的文案
@@ -102,6 +112,9 @@ public class CodeText extends androidx.appcompat.widget.AppCompatEditText {
         mCursorStrokeWidth=typedArray.getInt(R.styleable.CodeText_codeText_cursorStrokeWidth, mCursorStrokeWidth);//线粗细
         mCursorColor=typedArray.getColor(R.styleable.CodeText_codeText_cursorColor, mCursorColor);//颜色
         mCursorEnable=typedArray.getBoolean(R.styleable.CodeText_codeText_enableCursor,mCursorEnable);//开启关闭
+        mCursorHeightPadding=typedArray.getInt(R.styleable.CodeText_codeText_cursorHeightPadding,1);//高度边距
+        mCursorFrequency=typedArray.getInt(R.styleable.CodeText_codeText_cursorFrequencyMillisecond,mCursorFrequency);//闪烁频率
+
         typedArray.recycle();
         initial(context);
     }
@@ -160,7 +173,7 @@ public class CodeText extends androidx.appcompat.widget.AppCompatEditText {
         mCursorTimerTask = new TimerTask() {
             @Override
             public void run() {
-                mCursorDisplaying = !mCursorDisplaying;
+                mCursorDisplayingByTimer = !mCursorDisplayingByTimer;
                 postInvalidate();
             }
         };
@@ -204,7 +217,7 @@ public class CodeText extends androidx.appcompat.widget.AppCompatEditText {
             mBoxRectF.right = (float)(mBoxRectF.left + mBoxSize - (mBoxStrokeStyle == PAINT_STROKE || mHighLightStrokeStyle == PAINT_STROKE ?mBoxStrokeWidth:0 ));
             mBoxRectF.top =(float)( mBoxStrokeStyle == PAINT_STROKE|| mHighLightStrokeStyle == PAINT_STROKE ?mBoxStrokeWidth :0);
             mBoxRectF.bottom = (float)(viewHeight - (mBoxStrokeStyle == PAINT_STROKE|| mHighLightStrokeStyle == PAINT_STROKE ? mBoxStrokeWidth :0));
-            if(mCodeStyle==CODE_TEXT_STYLE_HIGHLIGHT && i == mHighLightIndex){
+            if(mEnableHighLight && i == mHighLightIndex){
                 mPaintBox.setColor(mHighLightBackgroundColor);
                 canvas.drawRoundRect(mBoxRectF, mHighLightRadius, mHighLightRadius, mHighLightPaint);
                 onDrawCursor(canvas,mCursorPaint,mBoxRectF);
@@ -223,13 +236,15 @@ public class CodeText extends androidx.appcompat.widget.AppCompatEditText {
     //绘制-光标
     private void onDrawCursor(Canvas canvas,Paint paint,RectF rectF){
         if(paint!=null && mCursorEnable){
-            if(mCursorDisplaying){
-                mCursorPaint.setColor(mCursorColor);
-            }else{
-                mCursorPaint.setColor(Color.TRANSPARENT);
-            }
-            canvas.drawRect((float)( (rectF.left+rectF.right)/2-mCursorStrokeWidth),(float)(mCursorStrokeWidth),(float)( (rectF.left+rectF.right)/2+mCursorStrokeWidth),(float)( rectF.bottom-mCursorStrokeWidth),paint);
+            mCursorPaint.setColor((mCursorDisplayingByTimer || mCursorDisplayingByIndex) ?mCursorColor:Color.TRANSPARENT);
+            canvas.drawRect(
+                    (float)((rectF.left+rectF.right)/2-mCursorStrokeWidth),
+                    (float)(mCursorHeightPadding<=1?(rectF.top+rectF.bottom)/4:mCursorHeightPadding),
+                    (float)( (rectF.left+rectF.right)/2+mCursorStrokeWidth),
+                    (float)( rectF.bottom-(mCursorHeightPadding<=1?(rectF.top+rectF.bottom)/4:mCursorHeightPadding))
+                    ,paint);
         }
+        mCursorDisplayingByIndex=false;
     }
 
     //检测输入内容，并画在画布上
@@ -249,6 +264,7 @@ public class CodeText extends androidx.appcompat.widget.AppCompatEditText {
                 }
             }
             postInvalidate();
+            this.mCursorDisplayingByIndex=true;
         }
     }
     @Override
@@ -256,7 +272,7 @@ public class CodeText extends androidx.appcompat.widget.AppCompatEditText {
         super.onAttachedToWindow();
         //cursorFlashTime为光标闪动的间隔时间
         if(mCursorEnable) {
-            mCursorTimer.scheduleAtFixedRate(mCursorTimerTask, 0, 500);
+            mCursorTimer.scheduleAtFixedRate(mCursorTimerTask, 0, mCursorFrequency);
         }
     }
 
